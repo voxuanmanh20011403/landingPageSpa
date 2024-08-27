106688e299961053ed4ce24628a3c8fb822e0d2d
Viet-Truong <viettruong0825@gmail.com>
2024-08-27 14:43:39 +0700

    styles home page

--- landing-page-spa/src/common/form-services/FormService.tsx
+++ landing-page-spa/src/common/form-services/FormService.tsx
@@ -1,16 +1,8 @@
-import React from "react";
-import "./styles.scss";
-import {
-  Button,
-  Checkbox,
-  DatePicker,
-  Form,
-  Input,
-  Radio,
-  Typography,
-} from "antd";
-import dayjs from "dayjs";
-import { ServicesList } from "../../pages/Services/Services.string";
+import React from 'react';
+import './styles.scss';
+import { Button, Checkbox, Col, DatePicker, Form, Input, Radio, Row, Typography } from 'antd';
+import dayjs from 'dayjs';
+import { ServicesList } from '../../pages/Services/Services.string';
 
 export default function FormService() {
   const [dateTime, setDateTime] = React.useState<dayjs.Dayjs | null>(null);
@@ -20,7 +12,7 @@ export default function FormService() {
   };
 
   const disabledDate = (current: dayjs.Dayjs) => {
-    return current && current < dayjs().startOf("day");
+    return current && current < dayjs().startOf('day');
   };
 
   const disabledTime = () => ({
@@ -38,125 +30,116 @@ export default function FormService() {
 
   const onFinish = (values: any) => {
     try {
-      const formattedDateTime = dayjs(values.DatePicker).format(
-        "HH:mm DD/MM/YYYY"
-      );
-      console.log("Form Data: ", formattedDateTime);
+      const formattedDateTime = dayjs(values.DatePicker).format('HH:mm DD/MM/YYYY');
+      console.log('Form Data: ', formattedDateTime);
     } catch (error) {
-      console.log("bug");
+      console.log('bug');
     }
   };
 
   const { Title } = Typography;
   return (
-    <div className="form-services">
-      <Title style={{ fontFamily: '"Playfair Display", serif' }}>
-        QUICK RESERVATION
-      </Title>
-      <Form layout="vertical" onFinish={onFinish}>
+    <div className='form-services'>
+      <Title style={{ fontFamily: '"Playfair Display", serif' }}>QUICK RESERVATION</Title>
+      <Form layout='vertical' onFinish={onFinish}>
         <Form.Item
-          label="Name"
-          name="name"
+          label='Name'
+          name='name'
           rules={[
-            { required: true, message: "Please input your name!" },
+            { required: true, message: 'Please input your name!' },
             {
               pattern: /^[\p{L}\s.,!?'-]+$/u,
               message:
-                "Name must contain only letters (including Vietnamese letters), spaces, and valid punctuation!",
+                'Name must contain only letters (including Vietnamese letters), spaces, and valid punctuation!',
             },
           ]}
         >
-          <Input placeholder="Full name" size="large" />
+          <Input placeholder='Full name' size='large' />
         </Form.Item>
 
         <Form.Item
-          label="Phone"
-          name="phone"
+          label='Phone'
+          name='phone'
           rules={[
-            { required: true, message: "Please input your phone number!" },
+            { required: true, message: 'Please input your phone number!' },
             {
               pattern: /^[0-9]{10}$/,
-              message: "Phone number must be 10 digits!",
+              message: 'Phone number must be 10 digits!',
             },
           ]}
         >
-          <Input placeholder="Number phone" size="large" />
+          <Input placeholder='Number phone' size='large' />
         </Form.Item>
 
         <Form.Item
-          label="Email"
-          name="email"
+          label='Email'
+          name='email'
           rules={[
-            { required: true, message: "Please input your email!" },
-            { type: "email", message: "The input is not valid E-mail!" },
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'The input is not valid E-mail!' },
           ]}
         >
-          <Input placeholder="Email contact" size="large" />
+          <Input placeholder='Email contact' size='large' />
         </Form.Item>
 
         <Form.Item
-          label="Number of People *"
-          name="people"
+          label='Number of People *'
+          name='people'
           rules={[
-            { required: true, message: "Please input the number of people!" },
+            { required: true, message: 'Please input the number of people!' },
             {
               pattern: /^[0-9]{1,2}$/,
-              message: "Number of people must be a valid number!",
+              message: 'Number of people must be a valid number!',
             },
           ]}
         >
-          <Input placeholder="How many people?" size="large" />
+          <Input placeholder='How many people?' size='large' />
         </Form.Item>
 
         <Form.Item
-          label="Date/Time Checkin "
-          name="DatePicker"
-          rules={[
-            { required: true, message: "Please select a date and time!" },
-          ]}
+          label='Date/Time Checkin '
+          name='DatePicker'
+          rules={[{ required: true, message: 'Please select a date and time!' }]}
         >
           <DatePicker
-            style={{ width: "100%" }}
+            style={{ width: '100%' }}
             showTime
-            format="YYYY-MM-DD HH:mm"
+            format='YYYY-MM-DD HH:mm'
             onChange={handleChange}
             value={dateTime}
             disabledDate={disabledDate}
             disabledTime={disabledTime}
-            size="large"
+            size='large'
           />
         </Form.Item>
+        <Row className='row'>
+          <Col span={12}>
+            <Form.Item name='checkbox' valuePropName='checked' label='Menu'>
+              {ServicesList.map((item) => (
+                <Checkbox key={item.title} value={item.title} className='menu_item'>
+                  {item.title}
+                </Checkbox>
+              ))}
+            </Form.Item>
+          </Col>
 
-        <Form.Item
-          name="checkbox"
-          valuePropName="checked"
-          className="form-menu"
-        >
-          {ServicesList.map((item) => (
-            <Checkbox
-              key={item.title}
-              value={item.title}
-              className="form-menu-item"
-            >
-              {item.title}
-            </Checkbox>
-          ))}
-        </Form.Item>
+          <Col span={12}>
+            <Form.Item name='duration' label={'Duration'}>
+              <Radio.Group className='wrap_radio'>
+                <Radio value='30'> 30 mins </Radio>
+                <Radio value='60'> 60 mins </Radio>
+                <Radio value='90'> 90 mins </Radio>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item name="duration">
-          <Radio.Group>
-            <Radio value="30"> 30 mins </Radio>
-            <Radio value="60"> 60 mins </Radio>
-            <Radio value="90"> 90 mins </Radio>
-          </Radio.Group>
-        </Form.Item>
-
-        <Form.Item label="Message" name="requirements">
-          <Input placeholder="Other requirements ?" size="large" />
+        <Form.Item label='Message' name='requirements'>
+          <Input placeholder='Other requirements ?' size='large' />
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-          <Button type="primary" htmlType="submit" className="blinking-button">
+          <Button type='primary' htmlType='submit' className='booking_btn'>
             BOOKING
           </Button>
         </Form.Item>
--- landing-page-spa/src/pages/home/component/number-customer/NumberCustomer.tsx
+++ landing-page-spa/src/pages/home/component/number-customer/NumberCustomer.tsx
@@ -1,21 +1,21 @@
-import { Col, Row } from "antd";
-import classNames from "classnames";
-import "./NumberCustomer.scss";
+import { Col, Row } from 'antd';
+import classNames from 'classnames';
+import './NumberCustomer.scss';
 
 export default function NumberCustomer() {
   return (
-    <Row className={classNames("number-customer")}>
+    <Row className={classNames('number-customer')}>
       <Col span={8}>
-        <span>365</span>
+        <p>365</p>
         <h3>Opening day of the year</h3>
       </Col>
       <Col span={8}>
-        <span>1,750</span>
-        <h6>Customer Satisfaction</h6>
+        <p>1,750</p>
+        <h3>Customer Satisfaction</h3>
       </Col>
       <Col span={8}>
-        <span>20</span>
-        <h6>Beds are available</h6>
+        <p>20</p>
+        <h3>Beds are available</h3>
       </Col>
     </Row>
   );
