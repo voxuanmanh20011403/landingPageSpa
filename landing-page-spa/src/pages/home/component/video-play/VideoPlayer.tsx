5f65d771ec1720a255edc600f524ab590149141d
voxuanmanh20011403 <dvfbxm@gmail.com>
2024-09-01 10:53:25 +0700

    fix video play in home

--- landing-page-spa/src/pages/home/component/video-play/VideoPlayer.tsx
+++ landing-page-spa/src/pages/home/component/video-play/VideoPlayer.tsx
@@ -1,10 +1,39 @@
+import React, { useRef, useEffect, useState } from 'react';
+
 interface IProp {
   src?: string;
 }
+
 const VideoPlay: React.FC<IProp> = ({ src }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); 
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <video width="90%" height="60%" autoPlay muted loop>
-      <source src={src} type="video/mp4" />
+    <video ref={videoRef} width="90%" height="60%" autoPlay muted loop>
+      {isVisible && <source src={src} type="video/mp4" />}
     </video>
   );
 };
